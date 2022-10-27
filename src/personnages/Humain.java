35e612ea960d75540ef0fffd754d189f1b2f5c45
joseph wilkens <jsw3430a@ens.fsi>
2022-10-27 15:26:54 +0200

    debut tp4

---
+++ src/personnages/Humain.java
@@ -0,0 +1,55 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boisson;
+	private int nbArgent;
+	
+	public Humain(String nom, String boisson, int nbArgent) {
+		this.nom = nom;
+		this.boisson = boisson;
+		this.nbArgent = nbArgent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getNbArgent() {
+		return nbArgent;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + texte + ".");
+	}
+	
+	private String prendreParole() {
+		return "(" + nom +")";
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour! Je m'appelle " + nom + " et j'aime boire du " + boisson);
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boisson + "! GLOUPS !");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (prix<=nbArgent) {
+			parler("J'ai " + nbArgent + " sous en poche. Je vais pouvoir m'offrir une boisson à " + prix + " sous");
+			perdreArgent(prix);
+		} else {
+			parler("Je n'ai plus que " + nbArgent + " sous en poche. Je ne peux même pas m'offrir un kimono à" + prix + " sous");
+		}
+	}
+	
+	public void gagnerArgent(int gain) {
+		nbArgent+=gain;
+	}
+	
+	public void perdreArgent(int perte) {
+		nbArgent-=perte;
+	}
+	
+}
